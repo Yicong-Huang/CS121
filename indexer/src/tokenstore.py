6395551480bc8cb8f966d5207444e79b819ed548
Yicong-Huang <hyc541978023@gmail.com>
2018-05-18 15:44:26 -0700

    implemented idf

--- indexer/src/tokenstore.py
+++ indexer/src/tokenstore.py
@@ -1,29 +1,32 @@
-import operator
-from collections import Counter
-from functools import reduce
+import math
 
 import redis
 
 
-def decode(str):
-    return str.decode('utf-8')
-
-
 class TokenStore:
-    def __init__(self):
-        self.r = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
-    def store_token(self, token, page, prefix='token'):
-        if self.r.zincrby('%s:%s' % (prefix, token), page) is False:
-            raise RuntimeError('Error: Failed to storeIndex(token: %s, page: %s)!', token, page)
+    @staticmethod
+    def decode(str):
+        return str.decode('utf-8')
 
-    def tokens(self, prefix='token'):
-        return (decode(x) for x in self.r.scan_iter('%s:*' % prefix))
+    def __init__(self, prefix='token'):
+        self._prefix = prefix
+        self._redis = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+    def store_token(self, token, page):
+        if not self._redis.zincrby(self.prefixed(token), page):
+            raise RuntimeError("Error: Failed to storeIndex(token: {}, page: {})!".format(token, page))
+
+    def tokens(self):
+        return (TokenStore.decode(x) for x in self._redis.scan_iter(self.prefixed("*")))
 
     def token_occurrence_pairs(self):
         for token in self.tokens():
-            occurrence = self.r.zrange(token, 0, -1, withscores=True)
-            yield (token, map(lambda x: (decode(x[0]), x[1]), occurrence))
+            occurrence = self._redis.zrange(token, 0, -1, withscores=True)
+            yield token, list(map(lambda x: (TokenStore.decode(x[0]), x[1]), occurrence))
+
+    def prefixed(self, token):
+        return "{}:{}".format(self._prefix, token)
 
     def get_tokens_on_page(self, page):
         return (key for key, val in self.token_occurrence_pairs() if str(page) in map(lambda x: x[0], val))
@@ -31,15 +34,20 @@ class TokenStore:
     def occurrences(self):
         return (pair[1] for pair in self.token_occurrence_pairs())
 
-    # def pages_count(self):
-    #     return reduce(operator.add, (Counter(x) for x in self.occurrences()))
+    def pages_count(self, token):
+        return self._redis.zcard(self.prefixed(token))
 
     def tf(self, token, page):
-        for pair in self.r.zrange(token, 0, -1, withscores=True):
-            if decode(pair[0]) == str(page):
+        for pair in self._redis.zrange(self.prefixed(token), 0, -1, withscores=True):
+            if TokenStore.decode(pair[0]) == str(page):
                 return pair[1]
         return 0
 
-    # def idf(self, token, page):
-    #     print(list(self.pages_count()))
-    #     pass
+    def idf(self, token):
+        return math.log(self.get_document_count() / self.pages_count(token))
+
+    def increment_document_count(self):
+        self._redis.set("document_count", self._redis.incr("document_count"))
+
+    def get_document_count(self):
+        return int(self._redis.get("document_count") or 0)
