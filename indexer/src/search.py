af15b94ba7840aad07f774783ea4bc9d4d5ddf8f
Yicong-Huang <hyc541978023@gmail.com>
2018-05-19 11:43:10 -0700

    default 10 output and --all arg

--- indexer/src/search.py
+++ indexer/src/search.py
@@ -4,16 +4,20 @@ from searchengine import SearchEngine
 from tokenstore import TokenStore
 
 
-def command_line():
-    command = input("Please type in what do you want to search: ").lower()
-    command = re.sub("[^A-Za-z0-9]+", "", command)
+def command_line(arg=None):
+    query = input("Please type in what do you want to search: ").lower()
+
+    if " --" in query and not arg:
+        query, arg = query.split(" --")
+
+    query = re.sub("[^A-Za-z0-9]+", "", query)
     valid = False
-    while valid != True:
-        if len(command) > 32:
+    while not valid:
+        if len(query) > 32:
             print("the query is too long, please shorten the search word")
         else:
             valid = True
-    return command
+    return query, arg
 
 
 if __name__ == '__main__':
@@ -21,5 +25,5 @@ if __name__ == '__main__':
     search_engine = SearchEngine(store)
 
     while True:
-        search_query = command_line()
-        search_engine.show_search(search_query)
+        search_query, arg = command_line()
+        search_engine.show_search(*(search_query, False) if arg == "all" else (search_query))
--- indexer/src/searchengine.py
+++ indexer/src/searchengine.py
@@ -7,11 +7,13 @@ class SearchEngine:
             decoded_page = self._token_store.decode(page)
             yield decoded_page[decoded_page.index(":") + 1:]
 
-    def show_search(self, query):
+    def show_search(self, query, limit=10):
 
         try:
             for index, result in enumerate(self.search(query), 1):
                 t = print(index, result)
+                if limit and index == limit:
+                    break
             t
         except:
             print("No Result")
