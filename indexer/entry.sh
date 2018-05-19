a770d922a0fc091594e4bf4f8c0f64998fe93330
Matthew Wo <9029537@gmail.com>
2018-05-19 08:00:39 -0700

    wait for redis to load first

--- indexer/entry.sh
+++ indexer/entry.sh
@@ -3,5 +3,8 @@
 # Start redis
 eval /redis/redis_init_script start
 
+# Wait for redis to load first
+until [ `redis-cli ping | grep -c PONG` = 1 ]; do echo "Waiting 1s for Redis to load"; sleep 1; done
+
 # Run CMD passed by docker
 exec "$@"
