c12bb0f368926f82d6c37be433626fb8f3a4bf3c
Matthew Wo <9029537@gmail.com>
2018-05-19 08:57:40 -0700

    faster redis load time

--- indexer/entry.sh
+++ indexer/entry.sh
@@ -7,4 +7,8 @@ eval /redis/redis_init_script start
 until [ `redis-cli ping | grep -c PONG` = 1 ]; do echo "Waiting 1s for Redis to load"; sleep 1; done
 
 # Run CMD passed by docker
-exec "$@"
+eval "$@"
+
+# Wair for at least 3 second before shutdown to let redis flush to disk
+echo "Shutting down... Wait for 3 seconds"
+sleep 3
