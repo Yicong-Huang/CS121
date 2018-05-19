ce1e1678f57915f79f1f521a97cb7ee57f8280c0
Zican Li <zicanl@uci.edu>
2018-05-19 03:15:37 -0700

    optimized search and HTML parser

--- indexer/src/HTML.py
+++ indexer/src/HTML.py
@@ -1,8 +1,7 @@
-import re
+
 from collections import Counter
-
+import re
 from bs4 import BeautifulSoup
-
 
 class Html:
     def __init__(self, path, url):
@@ -10,13 +9,24 @@ class Html:
         self.path = path
         self.url = url
         self.data = ''.join(open("./WEBPAGES_RAW/" + path, encoding="utf-8").readlines()).lower()
+        self.stop_words = open("stop_words.txt").readline().split(",")
         try:
-            self.data = BeautifulSoup(self.data, 'xml').get_text()
+            self.soup = BeautifulSoup(self.data, 'html.parser')
+        
+        #print(self.stop_words)
         except:
             print("not html")
 
     def tokens(self):
-        text = re.findall('[a-zA-Z\d]+', self.data)
+        #print(type(self.soup))
+        text = ''.join(self.soup.findAll(text = True))
+        text = re.sub("[^A-Z0-9a-z<> ]","",text)
+        text = re.sub("[<>]","",text)
+    
+
+        return Counter([word for word in text.split() if not word in self.stop_words])
+        #text = re.findall('[a-zA-Z\d]+', self.data)
+        #print(text)
         # Get token frequency with collections.Counter object, should be O(n) of time since it will be iterating entire
         # iterator of tokens.
-        return Counter(text)
+        #return Counter(text)
--- indexer/src/main.py
+++ indexer/src/main.py
@@ -1,18 +1,25 @@
 from indexer import Indexer
 from searchengine import SearchEngine
 from tokenstore import TokenStore
-
+import re
 
 def command_line():
-    return input("Please type in what do you want to search: ")
-
+    command = input("Please type in what do you want to search: ").lower()
+    command = re.sub("[^A-Za-z0-9]+","",command)
+    valid = False
+    while valid != True:
+        if len(command) > 32:
+            print("the query is too long, please shorten the search word")
+        else:
+            valid = True
+    return command
 
 if __name__ == '__main__':
     store = TokenStore()
     indexer = Indexer(store)
 
     book_file = open("WEBPAGES_RAW/bookkeeping.json")
-    indexer.run(book_file)
+    #indexer.run(book_file)
 
     search_engine = SearchEngine(store)
     search_query = command_line()
