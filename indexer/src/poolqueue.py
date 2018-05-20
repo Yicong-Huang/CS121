2c995413b1f45cfe23984e071f7cf582a32cddf3
Yicong-Huang <hyc541978023@gmail.com>
2018-05-20 08:10:33 -0700

    restructured with private access

--- indexer/src/poolqueue.py
+++ indexer/src/poolqueue.py
@@ -9,19 +9,19 @@ class PoolQueue:
     IDLE = 'idle'
     ACTIVE = 'active'
 
-    def __init__(self, rhost='127.0.0.1', rport=6379, rdb=0, store=None):
-        self.redis = redis.Redis(host=rhost, port=rport, db=rdb)
-        self.store = store
+    def __init__(self, rhost='127.0.0.1', rport=6379, rdb=0, token_store=None):
+        self._redis = redis.Redis(host=rhost, port=rport, db=rdb)
+        self._token_store = token_store
 
     def enqueue_idles(self, jobs, queue=IDLE):
         print("Enqueuing jobs...")
-        pipeline = self.redis.pipeline()
+        pipeline = self._redis.pipeline()
         for job in jobs:
             pipeline.rpush(queue, job)
         pipeline.execute()
 
     def get_next_job(self, f=IDLE, t=ACTIVE):
-        job_bytes = self.redis.rpoplpush(f, t)
+        job_bytes = self._redis.rpoplpush(f, t)
         if job_bytes is None:
             print("No more job available!")
         else:
@@ -31,4 +31,4 @@ class PoolQueue:
 
     def complete(self, job, queue=ACTIVE):
         print(threading.current_thread().getName(), "completed Job: %s" % job.path)
-        self.redis.lrem(queue, job)
+        self._redis.lrem(queue, job)
--- indexer/src/workerpool.py
+++ indexer/src/workerpool.py
@@ -8,32 +8,33 @@ from poolqueue import PoolQueue
 
 class WorkerPool:
     def __init__(self, token_store, workers, book_file):
-        self.workers = workers
-        self.book_file = book_file
-        self.work_queue = PoolQueue()
-        self.token_store = token_store
+        self._threads = []
+        self._workers = workers
+        self._book_file = book_file
+        self._work_queue = PoolQueue()
+        self._token_store = token_store
         self._setup(book_file)
 
     def _setup(self, file):
-        if not self.token_store.get_idle():
-            self.work_queue.enqueue_idles((Job(path, url) for path, url in json.load(file).items()))
+        if not self._token_store.get_idle():
+            self._work_queue.enqueue_idles((Job(path, url) for path, url in json.load(file).items()))
 
     def _worker(self):
-        indexer = Indexer(self.token_store)
+        indexer = Indexer(self._token_store)
         while True:
-            job = self.work_queue.get_next_job()
+            job = self._work_queue.get_next_job()
             if job is None:
                 break
             indexer.run(job.path, job.url)
-            self.work_queue.complete(job)
+            self._work_queue.complete(job)
 
     def execute(self):
-        threads = []
-        for _ in range(self.workers):
+        self._threads = []
+        for _ in range(self._workers):
             t = threading.Thread(target=self._worker)
             t.daemon = True
             t.start()
-            threads.append(t)
+            self._threads.append(t)
 
-        for thread in threads:
+        for thread in self._threads:
             thread.join()
