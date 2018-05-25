b63df58f2020428068f8d2d795999efdc1732023
Yicong-Huang <hyc541978023@gmail.com>
2018-05-25 14:20:06 -0700

    simple backend server

---
+++ Website/Server.py
@@ -0,0 +1,29 @@
+from flask import request
+from flask_api import FlaskAPI
+
+from searchengine import SearchEngine
+from tokenstore import TokenStore
+
+app = FlaskAPI(__name__)
+
+
+@app.route("/")
+def index():
+    return app.send_static_file('htmls/index.html')
+
+
+@app.route("/api/search", methods=["POST"])
+def search():
+    """
+    curl -X POST http://127.0.0.1:5000/api/search -d  queries="hello world"
+    """
+
+    queries = request.data.get("queries")
+    return search_engine.show_search(queries) or []
+
+
+if __name__ == "__main__":
+    store = TokenStore()
+    search_engine = SearchEngine(store)
+
+    app.run(debug=True)
