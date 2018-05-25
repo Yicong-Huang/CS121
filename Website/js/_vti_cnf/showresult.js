dc315e6ff0b2818237f73c05b5ae0af3495aa646
Zican Li <zicanl@uci.edu>
2018-05-24 21:44:46 -0700

    fronend website

---
+++ Website/js/_vti_cnf/showresult.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-N490TTB\\Varney
+vti_modifiedby:SR|DESKTOP-N490TTB\\Varney
+vti_timelastmodified:TR|24 May 2018 02:51:37 -0000
+vti_timecreated:TR|23 May 2018 19:06:45 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|search.html index.html
+vti_nexttolasttimemodified:TW|24 May 2018 02:51:07 -0000
+vti_cacheddtm:TX|24 May 2018 02:51:37 -0000
+vti_filesize:IR|1329
---
+++ Website/js/showresult.js
@@ -0,0 +1,53 @@
+﻿
+function handleSearchInput(){
+	let text = document.getElementsByName("Text")[0].value;
+	text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(" ",",");
+	console.log(text);
+	
+	return text;
+}
+
+
+function handleSearch(){
+	let text = handleSearchInput();
+	handleSearchResult();
+		
+	/*
+	jQuery.ajax{
+		dataType:"json",
+		url:"",
+		method:"GET",
+		success:(resultData)=>handleSearchResult(resultData)
+	}*/
+
+	
+}
+
+function handleSearchResult(){
+	let showResultElement = jQuery("#show_result");
+	showResultElement.empty();
+	let rowHtml = "";
+	for(let i = 0; i < 10; i++){
+		rowHtml += "<p id='url'><a href='www.baidu.com'>WWW.baidu.com</a></p>";
+	}
+	
+	showResultElement.append(rowHtml);
+
+}
+
+function getParameterByName(target) {
+        // Get request URL
+        let url = window.location.href;
+        // Encode target parameter name to url encoding
+        target = target.replace(/[\[\]]/g, "\\$&");
+
+        // Ues regular expression to find matched parameter value
+        let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+
+        // Return the decoded parameter value
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
