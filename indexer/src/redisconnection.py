658c9094e7d1ffdafb45d86f4cfc925a505c5df6
Matthew Wo <9029537@gmail.com>
2018-05-28 02:18:34 -0700

    clean up makefile

--- indexer/src/redisconnection.py
+++ indexer/src/redisconnection.py
@@ -2,8 +2,8 @@ import os
 from redis import StrictRedis
 
 def RedisConnection():
-    host = os.getenv('INDEXER_REDIS_HOST')
+    host = os.getenv('INDEXER_REDIS_HOST', '')
     host = host if len(host) > 0 else '127.0.0.1'
-    port = os.getenv('INDEXER_REDIS_PORT')
+    port = os.getenv('INDEXER_REDIS_PORT', '')
     port = int(port) if len(port) > 0 else 6379
     return StrictRedis(host=host, port=port, db=0, decode_responses=True)
