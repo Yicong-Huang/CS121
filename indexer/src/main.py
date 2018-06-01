7541963a4a727847f16182f5c4a9fbbd0a67db7d
Yicong-Huang <hyc541978023@gmail.com>
2018-06-01 10:14:46 -0700

    added to load bookkeeping as a hmset in redis

--- indexer/src/main.py
+++ indexer/src/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import signal
 
@@ -5,8 +6,11 @@ from tokenstore import TokenStore
 from workerpool import WorkerPool
 
 if __name__ == '__main__':
-    pool = WorkerPool(TokenStore(), workers=32, book_file=open("../WEBPAGES_RAW/bookkeeping.json", 'r'),
-                      mode=os.environ.get('INDEXER_MODE', 'SERVER'))
+    bookkeeping = json.load(open("../WEBPAGES_RAW/bookkeeping.json", 'r'))
+    store = TokenStore()
+    store.store_bookkeeping(bookkeeping)
+    pool = WorkerPool(TokenStore(), workers=32, bookkeeping=bookkeeping,
+                      mode=os.environ.get("INDEXER_MODE", "SERVER"))
     # capture signal.SIGINT and handle it with safe termination
     signal.signal(signal.SIGINT, lambda _s, _f: pool.safe_terminate())
     pool.execute()
