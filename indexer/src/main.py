3bb2277ae978cf83de6df258fe78dcda83a21d15
Yicong-Huang <hyc541978023@gmail.com>
2018-06-01 10:49:15 -0700

    changed corresponding to workerpool rename

--- indexer/src/main.py
+++ indexer/src/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     bookkeeping = json.load(open("../WEBPAGES_RAW/bookkeeping.json", 'r'))
     store = TokenStore()
     store.store_bookkeeping(bookkeeping)
-    pool = WorkerPool(TokenStore(), workers=32, bookkeeping=bookkeeping,
+    pool = WorkerPool(TokenStore(), worker_num=32, bookkeeping=bookkeeping,
                       mode=os.environ.get("INDEXER_MODE", "SERVER"))
     # capture signal.SIGINT and handle it with safe termination
     signal.signal(signal.SIGINT, lambda _s, _f: pool.safe_terminate())
