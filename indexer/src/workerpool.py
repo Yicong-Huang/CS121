686bbca01805eb2d5bdceacefa7b9737cd284fd7
Yicong-Huang <hyc541978023@gmail.com>
2018-06-01 10:48:40 -0700

    renamed and polished prints logic

--- indexer/src/workerpool.py
+++ indexer/src/workerpool.py
@@ -1,4 +1,3 @@
-import json
 import threading
 import time
 
@@ -8,21 +7,18 @@ from poolqueue import PoolQueue
 from redisconnection import RedisConnection
 
 
-# Todo: add logging to log workers' activities
-
 class WorkerPool:
     SERVER_MODE = "SERVER"
     CLIENT_MODE = "CLIENT"
 
-    def __init__(self, token_store, workers, book_file, mode):
-        self._threads = []
-        self._workers = workers
-        self._book_file = book_file
+    def __init__(self, token_store, worker_num, bookkeeping: dict, mode):
+        self._workers = set()
+        self._worker_num = worker_num
         self._redis = RedisConnection.shared().get_connection()
         self._work_queue = PoolQueue()
         self._token_store = token_store
         self._mode = mode
-        self._setup(book_file)
+        self._setup(bookkeeping)
         self._running = True
         self._running_thread = None
 
@@ -32,14 +28,14 @@ class WorkerPool:
             pipeline.rpoplpush(PoolQueue.ACTIVE, PoolQueue.IDLE)
             pipeline.execute()
 
-    def _setup(self, file):
+    def _setup(self, bookkeeping):
         if self._mode == WorkerPool.CLIENT_MODE:
             return
 
         if not any([self._work_queue.indexer_jobs_completed(),
                     self._work_queue.has_idle_job(),
                     self._work_queue.has_active_job()]):
-            self._work_queue.enqueue_idles((Job(path, url) for path, url in self._sort_jobs(json.load(file))))
+            self._work_queue.enqueue_idles((Job(path, url) for path, url in self._sort_jobs(bookkeeping)))
 
         self._reenqueue_active()  # move all active jobs to idle on startup
 
@@ -60,9 +56,9 @@ class WorkerPool:
 
         def listener():
             pubsub = self._redis.pubsub()
-            pubsub.subscribe('indexer:clients')
+            pubsub.subscribe("indexer:clients")
             for message in pubsub.listen():
-                if message.get('data', '') == 'TERMINATE':
+                if message.get("data") == "TERMINATE":
                     print("!!! RECEIVED TERMINATE FROM SERVER !!!")
                     self.safe_terminate()
                     break
@@ -71,21 +67,21 @@ class WorkerPool:
         listener_thread.daemon = True
         listener_thread.start()
 
-        for i in range(self._workers):
-            t = threading.Thread(target=worker, name='Worker ' + str(i + 1))
+        for i in range(self._worker_num):
+            t = threading.Thread(target=worker, name="Worker-{num}".format(num=i + 1))
             t.start()
-            self._threads.append(t)
+            self._workers.add(t)
 
     def _execute_server(self):
         def server():
             while self._running:
                 active_jobs = self._work_queue.get_active()
                 idle_jobs = self._work_queue.get_idle()
-                clients = self._redis.pubsub_numsub('indexer:clients')[0][1]
-                print('Number of active jobs: %d, number of idle jobs: %d, number of clients: %d' % (
+                clients = self._redis.pubsub_numsub("indexer:clients")[0][1]
+                print("Number of active jobs: {}, number of idle jobs: {}, number of clients: {}".format(
                     len(active_jobs), len(idle_jobs), clients))
-                if clients == 0 and active_jobs:
-                    print('No more clients connected. Re-enqueuing active jobs to idle...')
+                if not clients and active_jobs:
+                    print("No more clients connected. Re-enqueuing active jobs to idle...")
                     self._reenqueue_active()
                 time.sleep(1)
 
@@ -93,7 +89,7 @@ class WorkerPool:
         self._running_thread.start()
 
     def execute(self):
-        print('Executing as ' + self._mode)
+        print("Executing as {mode}".format(mode=self._mode))
         if self._mode == WorkerPool.CLIENT_MODE:
             self._execute_client()
         elif self._mode == WorkerPool.SERVER_MODE:
@@ -105,19 +101,18 @@ class WorkerPool:
         def terminator():
             if self._mode == WorkerPool.SERVER_MODE:
                 while True:
-                    clients = self._redis.pubsub_numsub('indexer:clients')[0][1]
+                    clients = self._redis.pubsub_numsub("indexer:clients")[0][1]
                     if clients == 0:
                         return
-                    self._redis.publish('indexer:clients', 'TERMINATE')
-                    print("Waiting for %d clients to disconnect..." % clients)
+                    self._redis.publish("indexer:clients", "TERMINATE")
+                    print("Waiting for {} clients to disconnect...".format(clients))
                     time.sleep(1)
             elif self._mode == WorkerPool.CLIENT_MODE:
-                for i, thread in enumerate(self._threads):
-                    name, rest_worker_count = thread.getName(), self._workers - (i + 1)
-                    print("Attempting to join {thread_name}, still waiting for {count} workers...".format(
-                        thread_name=name, count=rest_worker_count))
-                    thread.join()
-                    print("Joined {thread}, still waiting for {num} workers".format(thread=name, num=rest_worker_count))
+                while self._workers:
+                    worker = self._workers.pop()
+                    worker.join()
+                    print("Joined {worker}".format(worker=worker.getName()), "still waiting for {num} workers".format(
+                        num=len(self._workers)) if self._workers else "all workers joined")
 
         terminator_thread = threading.Thread(target=terminator)
         terminator_thread.start()
@@ -126,4 +121,4 @@ class WorkerPool:
         if self._running_thread:
             self._running_thread.join()
 
-        print("Terminating %s now." % self._mode)
+        print("Terminating {mode} now.".format(mode=self._mode))
